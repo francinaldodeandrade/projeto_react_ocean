61ec43fa1f3ac162886cfe086b15a09564ef9469
Naldo de Andrade <106287645+francinaldodeandrade@users.noreply.github.com>
2023-09-27 23:47:56 -0300

    organização dos cards

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,24 @@ function App() {
     nome: "Beth Smith",
     url_img: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
   };
+  const item5 = {
+    nome: "Rick Sanchez",
+    url_img: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+  };
+  const item6 = {
+    nome: "Morty Smith",
+    url_img: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+  };
+  const item7 = {
+    nome: "Summer Smith",
+    url_img: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+  };
+  const item8 = {
+    nome: "Beth Smith",
+    url_img: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+  };
 
-  const itens = [item1, item2, item3, item4];
+  const itens = [item1, item2, item3, item4, item5, item6, item7, item8];
 
   return (
     <>
